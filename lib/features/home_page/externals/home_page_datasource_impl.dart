348796bf5af7ec6fc557938a1d43e699eda9cb4b
EdsonMello-code <edsonmelo754@gmail.com>
2022-01-08 12:29:31 -0300

    feat: add test of home_datasource_test

--- lib/features/home_page/externals/home_page_datasource_impl.dart
+++ lib/features/home_page/externals/home_page_datasource_impl.dart
@@ -1,18 +1,21 @@
 import 'package:app_food/features/home_page/domain/entities/location.dart';
 import 'package:app_food/features/home_page/domain/errors/location_error.dart';
+import 'package:app_food/features/home_page/externals/geocoding_adpter.dart';
 import 'package:app_food/features/home_page/infra/datasources/home_page_datasource.dart';
 import 'package:geocoding/geocoding.dart' as geocoding hide Location;
 import 'package:location/location.dart';
 
 class HomePageDatasourceImpl implements HomePageDatasource {
   final Location location;
+  final GeocodingAdpter geocoding;
 
-  HomePageDatasourceImpl({required this.location});
+  HomePageDatasourceImpl({required this.location, required this.geocoding});
 
   @override
   Future<LocationEntity> geCurrentLocation() async {
     try {
       final latLong = await location.getLocation();
+      print(latLong);
       if (latLong.latitude == null || latLong.longitude == null) {
         throw LocationFailure(
           message:
---
+++ test/features/home_page/externals/home_datasource_test.dart
@@ -0,0 +1,111 @@
+import 'package:app_food/features/home_page/externals/geocoding_adpter.dart';
+import 'package:app_food/features/home_page/externals/home_page_datasource_impl.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geocoding/geocoding.dart' hide Location;
+import 'package:mocktail/mocktail.dart';
+import 'package:location/location.dart';
+
+void main() {
+  group('Datasource: ', () {
+    test(
+        'Should have fech location of datasource then call method placemarkFromCoordinates',
+        () async {
+      final geocodingAdapterMock = GeocodingAdapterMock();
+      final locationMock = LocationMock();
+
+      final homeDatasource = HomePageDatasourceImpl(
+        location: locationMock,
+        geocoding: geocodingAdapterMock,
+      );
+
+      Future<LocationDataMock> getLocation() async => LocationDataMock();
+
+      when(
+        () => geocodingAdapterMock.placemarkFromCoordinates(
+          any(),
+          any(),
+        ),
+      ).thenAnswer((_) async => [
+            Placemark(
+              subLocality: 'Mata Grande',
+            )
+          ]);
+
+      when(
+        () => locationMock.getLocation(),
+      ).thenAnswer((_) => getLocation());
+
+      final location = await homeDatasource.geCurrentLocation();
+
+      expect(location.lat, equals(2.2));
+      expect(location.city, equals('Mata Grande'));
+      expect(location.long, equals(2.2));
+    });
+  });
+}
+
+class GeocodingAdapterMock extends Mock implements GeocodingAdpter {}
+
+class LocationMock extends Mock implements Location {}
+
+class LocationDataMock implements LocationData {
+  @override
+  // TODO: implement accuracy
+  double? get accuracy => 2.2;
+
+  @override
+  // TODO: implement altitude
+  double? get altitude => 2.2;
+
+  @override
+  // TODO: implement elapsedRealtimeNanos
+  double? get elapsedRealtimeNanos => 2.2;
+
+  @override
+  // TODO: implement elapsedRealtimeUncertaintyNanos
+  double? get elapsedRealtimeUncertaintyNanos => 2.2;
+
+  @override
+  // TODO: implement heading
+  double? get heading => 2.2;
+
+  @override
+  // TODO: implement headingAccuracy
+  double? get headingAccuracy => 2.2;
+
+  @override
+  // TODO: implement isMock
+  bool? get isMock => true;
+
+  @override
+  // TODO: implement latitude
+  double? get latitude => 2.2;
+
+  @override
+  // TODO: implement longitude
+  double? get longitude => 2.2;
+
+  @override
+  // TODO: implement provider
+  String? get provider => '';
+
+  @override
+  // TODO: implement satelliteNumber
+  int? get satelliteNumber => 2;
+
+  @override
+  // TODO: implement speed
+  double? get speed => 2.2;
+
+  @override
+  // TODO: implement speedAccuracy
+  double? get speedAccuracy => 2.2;
+
+  @override
+  // TODO: implement time
+  double? get time => 2.2;
+
+  @override
+  // TODO: implement verticalAccuracy
+  double? get verticalAccuracy => 2.2;
+}
